59a6a70dadf327194ad8d5f6c34687d9e166b119
Peeter Joot <peeter.joot@lzlabs.com>
2016-12-22 23:55:59 -0500

    modified:   pauliGA.m

--- pauliGA.m
+++ pauliGA.m
@@ -1,19 +1,8 @@
 (* ::Package:: *)
 
-(*
 Begin["`Private`"]
-End[]
-*)
-
-Begin["pauliGA`"]
-
-ClearAll[ scalarType, vectorType, bivectorType, trivectorType, multivectorType, Scalar, Vector, Bivector, Trivector ]
+ClearAll[ scalarType, vectorType, bivectorType, trivectorType, multivectorType ]
 {scalarType, vectorType, bivectorType, trivectorType, multivectorType} = Range[5];
-
-Scalar[v_] := {scalarType, v IdentityMatrix[2]};
-Vector[v_, k_Integer /; k > 0 && k < 4] := {vectorType, v PauliMatrix[k] };
-Bivector[v_, k_Integer /; k > 0 && k < 4, j_Integer  /; j > 0 && j < 4] := {bivectorType, v PauliMatrix[k] . PauliMatrix[j] };
-Trivector[v_] := {trivectorType, v I IdentityMatrix[2]};
 
 ClearAll[ directProduct, signedSymmetric, symmetric, antisymmetric ]
 
@@ -31,8 +20,40 @@ pBivectorQ[m : {bivectorType, _}] := True ;
 pBivectorQ[m : {_Integer, _}] := False ;
 pTrivectorQ[m : {trivectorType, _}] := True ;
 pTrivectorQ[m : {_Integer, _}] := False ;
+pMultivectorQ[m : {multivectorType, _}] := True ;
+pMultivectorQ[m : {_Integer, _}] := False ;
 pBladeQ[m : {multivectorType, _}] := False ;
 pBladeQ[m : {_Integer, _}] := True ;
+
+ClearAll[ grade0, grade1, grade2, grade3, grade01, grade23 ]
+grade0 := IdentityMatrix[2] ( #/2 // Tr // Re // Simplify) &;
+grade3 := I IdentityMatrix[2] ( #/2 // Tr // Im // Simplify) &;
+grade01 := (((# + (# // ConjugateTranspose))/2) // Simplify)  &;
+grade23 := (((# - (# // ConjugateTranspose))/2) // Simplify)  &;
+grade1 := ((grade01[#] - grade0[#]) // Simplify) &;
+grade2 := ((grade23[#] - grade3[#]) // Simplify) &;
+
+ClearAll[ gaPlus, gaminus, magnitude ]
+
+gaPlus[ v1_, v2_, f1_ : 1, f2_ : 1 ] := Module[ 
+   {g1 = (v1 // First), g2 = (v2 // First), a = (v1 // Last), b = (v2 // Last)},
+   { If[ g1 == g2, g1, multivectorType ],
+   f1 a + f2 b} ]
+gaMinus[ v1_, v2_ ] := gaPlus[ v1, v2, 1, -1 ] ;
+gaNegate[ v_ ] := { v // First, - (v // Last) } ;
+
+magnitude[ v_?pScalarQ ] := v[[1,1]] ;
+End[]
+
+(* copy this module to a directory in $Path.  Then invoke with <<peeters` *)
+Begin["pauliGA`"]
+
+ClearAll[ Scalar, Vector, Bivector, Trivector ]
+
+Scalar[v_] := {scalarType, v IdentityMatrix[2]};
+Vector[v_, k_Integer /; k > 0 && k < 4] := {vectorType, v PauliMatrix[k] };
+Bivector[v_, k_Integer /; k > 0 && k < 4, j_Integer  /; j > 0 && j < 4] := {bivectorType, v PauliMatrix[k] . PauliMatrix[j] };
+Trivector[v_] := {trivectorType, v I IdentityMatrix[2]};
 
 ClearAll[ DotProduct, WedgeProduct, GeometricProduct ]
 DotProduct[ v1_?pScalarQ, v2_?pScalarQ]    := directProduct[ scalarType, v1 , v2  ] ;
@@ -77,14 +98,7 @@ WedgeProduct[ v1_?pTrivectorQ, v2_?pTrivectorQ ] := Scalar[0] ;
 
 GeometricProduct[ v1_, v2_ ] := directProduct[ multivectorType, v1, v2 ] ;
 
-ClearAll[ grade0, grade1, grade2, grade3, grade01, grade23, GradeSelection, ScalarSelection, PseudoScalarSelection ]
-grade0 := IdentityMatrix[2] ( #/2 // Tr // Re // Simplify) &;
-grade3 := I IdentityMatrix[2] ( #/2 // Tr // Im // Simplify) &;
-grade01 := (((# + (# // ConjugateTranspose))/2) // Simplify)  &;
-grade23 := (((# - (# // ConjugateTranspose))/2) // Simplify)  &;
-grade1 := ((grade01[#] - grade0[#]) // Simplify) &;
-grade2 := ((grade23[#] - grade3[#]) // Simplify) &;
-
+ClearAll[ GradeSelection, ScalarSelection, PseudoScalarSelection ]
 GradeSelection[ v_, n_Integer /; n == 0 ] := Scalar[ (v // Last) // grade0 ] ;
 GradeSelection[ v_, n_Integer /; n == 1 ] := Vector[ (v // Last) // grade1 ] ;
 GradeSelection[ v_, n_Integer /; n == 2 ] := Bivector[ (v // Last) // grade2 ] ;
@@ -94,22 +108,12 @@ VectorSelection[ v_ ] := GradeSelection[ v, 1 ] ;
 BivectorSelection[ v_ ] := GradeSelection[ v, 2 ] ;
 PseudoScalarSelection[ v_ ] := GradeSelection[ v, 3 ] ;
 
-ClearAll[ gaplus, gaminus, magnitude ]
-
-gaplus[ v1_, v2_, f1_ : 1, f2_ : 1 ] := Module[ 
-   {g1 = (v1 // First), g2 = (v2 // First), a = (v1 // Last), b = (v2 // Last)},
-   { If[ g1 == g2, g1, multivectorType ],
-   f1 a + f2 b} ]
-gaminus[ v1_, v2_ ] := gaplus[ v1, v2, 1, -1 ] ;
-ganegate[ v_ ] := { v // First, - (v // Last) } ;
-
-magnitude[ v_?pScalarQ ] := v[[1,1]] ;
 DotProduct[ v1_?pMultivectorQ, v2_?pScalarQ ] := directproduct[ multivectorType, v1, v2 ] ;
 DotProduct[ v1_?pMultivectorQ, v2_?pBladeQ ] := Module[{g0, g1, g2, g3},
    {g0, g1, g2, g3} = GradeSelection[v1, #] &/@ (Range[4]-1);
-   gaplus[ 
-      gaplus[ 
-         gaplus[
+   gaPlus[ 
+      gaPlus[ 
+         gaPlus[
             DotProduct[ g0, v2 ],
             DotProduct[ g1, v2 ]
          ],
@@ -120,9 +124,9 @@ DotProduct[ v1_?pMultivectorQ, v2_?pBladeQ ] := Module[{g0, g1, g2, g3},
 ]
 DotProduct[ v1_, v2_?pMultivectorQ ] := Module[{g0, g1, g2, g3},
    {g0, g1, g2, g3} = GradeSelection[v2, #] &/@ (Range[4]-1);
-   gaplus[ 
-      gaplus[ 
-         gaplus[
+   gaPlus[ 
+      gaPlus[ 
+         gaPlus[
             DotProduct[ v1, g0 ],
             DotProduct[ v1, g1 ]
          ],
